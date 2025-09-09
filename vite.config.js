ef57e58aab2f01a2b6c02fcf8477b30f2cb06648
osqat <osqatt@gmail.com>
2025-09-09 20:47:18 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',  <- optional if you want relative paths in build,
   server: {
     host: '0.0.0.0',
     port: 3000
